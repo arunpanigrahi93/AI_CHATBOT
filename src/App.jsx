f3d188772729972af7223649a5a7ec64934dd5af
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-09-11 20:29:37 +0530

    basic chatbot icon with text

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import ChatbotIcon from "./components/ChatbotIcon";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <div className="chatbot-popup">
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="logo-text">Chatbot</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
